e690e83ea3a05b6c8952d8e761c1e376e80f6310
rishi3qrw43 <128740474+rishi3qrw43@users.noreply.github.com>
2026-07-06 11:45:25 -0500

    Initial Commit

---
+++ SMFR2/SMFR2App.swift
@@ -0,0 +1,17 @@
+//
+//  SMFR2App.swift
+//  SMFR2
+//
+//  Created by Rishi Vora on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SMFR2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
